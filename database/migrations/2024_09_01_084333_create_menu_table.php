3ec9e009ce97914dc3b684f597ff4e3211c75589
hoeleong20 <chewhl2002@gmail.com>
2024-09-16 11:12:30 +0800

    Update menu table migration columns

--- database/migrations/2024_09_01_084333_create_menu_table.php
+++ database/migrations/2024_09_01_084333_create_menu_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('menu', function (Blueprint $table) {
             $table->id();
+            $table->string('menu_code');
+            $table->string('name');
+            $table->string('desc');
+            $table->string('');
             $table->timestamps();
         });
     }
